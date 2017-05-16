65248dae41149705fc9f25d67a2968964a5a3ec8
andraserban <andra.serban2095@gmail.com>
2017-05-16 22:07:51 +0300

    -integration

--- SportItems/js/products.js
+++ SportItems/js/products.js
@@ -1,13 +1,93 @@
-function loadProducts()
+function loadBasketball()
 {
     var sportItemsAPI = new sportItemsAPI();
     sportItemsAPI.setBaseURL("http://localhost:50311");
 
-    sportItemsAPI.getAllProducts().done(function (response)
-	{
-		
-		
-	}
-	
-	);
+    sportItemsAPI.getAllBasketball().done(
+        function (data){
+            var htmlString = "";
+            for(i = 0; i < data.length; i++){
+
+                if(data[i].Image == null){
+
+                    if(data[i].PriductId == null){
+
+                        htmlString += '<a class="img" href="Basketball.html"><img src=' + data[i].Image + 'class="img-responsive"></a>'
+                    }
+                    
+                } 
+            }
+            productBox.prepend(htmlString);
+        }
+    );
+}
+
+function loadSwimming()
+{
+    var sportItemsAPI = new sportItemsAPI();
+    sportItemsAPI.setBaseURL("http://localhost:50311");
+
+    sportItemsAPI.getAllSwimming().done(
+        function (data){
+            var htmlString = "";
+            for(i = 0; i < data.length; i++){
+
+                if(data[i].Image == null){
+
+                    if(data[i].PriductId == null){
+
+                        htmlString += '<a class="img" href="Swimming.html"><img src=' + data[i].Image + 'class="img-responsive"></a>'
+                    }
+                    
+                } 
+            }
+            productBox.prepend(htmlString);
+        }
+    );
+}
+function loadYoga()
+{
+    var sportItemsAPI = new sportItemsAPI();
+    sportItemsAPI.setBaseURL("http://localhost:50311");
+
+    sportItemsAPI.getAllYoga().done(
+        function (data){
+            var htmlString = "";
+            for(i = 0; i < data.length; i++){
+
+                if(data[i].Image == null){
+
+                    if(data[i].PriductId == null){
+
+                        htmlString += '<a class="img" href="Yoga.html"><img src=' + data[i].Image + 'class="img-responsive"></a>'
+                    }
+                    
+                } 
+            }
+            productBox.prepend(htmlString);
+        }
+    );
+}
+function loadPilates()
+{
+    var sportItemsAPI = new sportItemsAPI();
+    sportItemsAPI.setBaseURL("http://localhost:50311");
+
+    sportItemsAPI.getAllPilates().done(
+        function (data){
+            var htmlString ="";
+            for(i = 0; i < data.length; i++){
+
+                if(data[i].Image == null){
+
+                    if(data[i].PriductId == null){
+
+                        htmlString += '<a class="img" href="Pilates.html"><img src=' + data[i].Image + 'class="img-responsive"></a>'
+                    }
+                    
+                } 
+            }
+            productBox.prepend(htmlString);
+        }
+    );
 }
---
+++ WADserver/SQLQuery2.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO ProductTable(ProductId, productName, productCategory,price,description,image)
+VALUES ('1', 'test', 'test','100','test','');
